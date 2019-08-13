abe2853d500776e4b9316d5197653b0762cf410b
ClAndrew <cmlandrews@gmail.com>
2019-08-12 21:58:08 -0700

    Fix L06 and add test cases

--- L06.f90
+++ L06.f90
@@ -3,30 +3,56 @@
     program Exercises
     implicit none
     logical :: r
-    character(len=16) :: str
+    character(len=16) :: testStr
     
-    str = "Test"
-    r = isPalindrome(str)
+    testStr = 'racecar'
+    call doTestCase(testStr)
     
+    testStr = 'a'
+    call doTestCase(testStr)
+    
+    testStr = 'coffee'
+    call doTestCase(testStr)
     
     contains
+    
+    subroutine doTestCase(str)
+        character(len=16), intent(in) :: str
+        logical :: res
+        
+        res = isPalindrome(str)
+        if (res) then
+            print '(A, A)', trim(str), ' is a palindrome.'
+        else
+            print '(A, A)', trim(str), ' is not a palindrome.'
+        end if
+    
+    end subroutine doTestCase
+    
     function isPalindrome(str) result(r)
         character(len=16) :: str
         logical :: r
-        integer :: i
+        integer :: left
+        integer :: right
         integer :: strlen
         integer :: halflen
+        character :: leftCh, rightCh
         
-        strlen = len(strlen)
+        strlen = len_trim(str)
         halflen = strlen / 2
         
-        do i=1, halflen
-            if (str(i) .ne. str(len(str)-i+1)) then
+        do left=1, halflen
+            right = strlen - left + 1
+            leftCh = str(left:left+1)
+            rightCh = str(right:right+1)
+            if (leftCh .ne. rightCh) then
                 r = .false.
                 return
             end if
         end do
-        r = .true.        
+        
+        r = .true.
+        
     end function isPalindrome
     
     end program Exercises
